7ee91155ac6cfb323a0ae0c0c2975f6ea79d56ee
mkroetzsch <accounts@korrekt.org>
2017-10-12 12:03:39 +0200

    updates

--- Vorlesungen/build.sh
+++ Vorlesungen/build.sh
@@ -4,20 +4,24 @@
 #
 # Usage sh build.sh <lectureNumberAsString>
 
+fileprefix="FS2017-Vorlesung-" # the start of generated PDFs
 tmpfilename="tmplecturefile" # don't have any files called like this!
+
+printfilename=$fileprefix$1-print.pdf
+overlayfilename=$fileprefix$1-overlay.pdf
 
 echo "\documentclass[onlymath,handout]{beamer}\n" > $tmpfilename.tex
 tail -n +3 lecture-$1.tex >> $tmpfilename.tex
 pdflatex $tmpfilename.tex
 pdflatex $tmpfilename.tex
-pdfnup --nup 2x2 --outfile lecture-$01-print.pdf $tmpfilename.pdf
+pdfnup --nup 2x2 --outfile $printfilename $tmpfilename.pdf
 
 echo "\documentclass[onlymath]{beamer}\n" > $tmpfilename.tex
 tail -n +3 lecture-$1.tex >> $tmpfilename.tex
 pdflatex $tmpfilename.tex
 pdflatex $tmpfilename.tex
-mv $tmpfilename.pdf lecture-$1-overlay.pdf
+mv $tmpfilename.pdf $overlayfilename
 
 rm $tmpfilename.*
 
-echo "\n\n*** Finished creating files 'lecture-$1-overlay.pdf' and 'lecture-$01-print.pdf' ***\n"
+echo "\n\n*** Finished creating files '$overlayfilename' and '$printfilename' ***\n"
