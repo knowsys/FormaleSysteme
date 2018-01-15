c32815f6734b8b878a903e0ae473025888728bdc
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2018-01-15 11:13:53 +0100

    fix: improve build.sh portability
    `echo` does not support escape sequences such as "\n" on all systems (it
    fails on ArchLinux for example). Switching to `printf` solves this.

--- Vorlesungen/build.sh
+++ Vorlesungen/build.sh
@@ -10,13 +10,13 @@ tmpfilename="tmplecturefile" # don't have any files called like this!
 printfilename=$fileprefix$1-print.pdf
 overlayfilename=$fileprefix$1-overlay.pdf
 
-echo "\documentclass[aspectratio=1610,onlymath,handout]{beamer}\n" > $tmpfilename.tex
+printf "\documentclass[aspectratio=1610,onlymath,handout]{beamer}\n\n" > $tmpfilename.tex
 tail -n +3 lecture-$1.tex >> $tmpfilename.tex
 pdflatex $tmpfilename.tex
 pdflatex $tmpfilename.tex
 pdfnup --nup 2x2 --outfile $printfilename $tmpfilename.pdf
 
-echo "\documentclass[aspectratio=1610,onlymath]{beamer}\n" > $tmpfilename.tex
+printf "\documentclass[aspectratio=1610,onlymath]{beamer}\n\n" > $tmpfilename.tex
 tail -n +3 lecture-$1.tex >> $tmpfilename.tex
 pdflatex $tmpfilename.tex
 pdflatex $tmpfilename.tex
@@ -24,4 +24,4 @@ mv $tmpfilename.pdf $overlayfilename
 
 rm $tmpfilename.*
 
-echo "\n\n*** Finished creating files '$overlayfilename' and '$printfilename' ***\n"
+printf "\n\n*** Finished creating files '$overlayfilename' and '$printfilename' ***\n\n"
