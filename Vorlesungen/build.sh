e8b069b61d5a4a4b5ba9a79428277c811917ef15
Maximilian Marx <maximilian.marx@tu-dresden.de>
2018-02-20 11:58:38 +0100

    Correctly escape \

--- Vorlesungen/build.sh
+++ Vorlesungen/build.sh
@@ -10,13 +10,13 @@ tmpfilename="tmplecturefile" # don't have any files called like this!
 printfilename=$fileprefix$1-print.pdf
 overlayfilename=$fileprefix$1-overlay.pdf
 
-printf "\documentclass[aspectratio=1610,onlymath,handout]{beamer}\n\n" > $tmpfilename.tex
+printf "\\documentclass[aspectratio=1610,onlymath,handout]{beamer}\n\n" > $tmpfilename.tex
 tail -n +3 lecture-$1.tex >> $tmpfilename.tex
 pdflatex $tmpfilename.tex
 pdflatex $tmpfilename.tex
 pdfnup --nup 2x2 --outfile $printfilename $tmpfilename.pdf
 
-printf "\documentclass[aspectratio=1610,onlymath]{beamer}\n\n" > $tmpfilename.tex
+printf "\\documentclass[aspectratio=1610,onlymath]{beamer}\n\n" > $tmpfilename.tex
 tail -n +3 lecture-$1.tex >> $tmpfilename.tex
 pdflatex $tmpfilename.tex
 pdflatex $tmpfilename.tex
